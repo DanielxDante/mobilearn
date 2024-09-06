30fea855ae1fcd2438c59074a112466c6d013ff9
DanielxDante <danielonline170600@outlook.com>
2024-09-06 20:31:18 +0800

    splash screen

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,7 +1,30 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Inter": require("../assets/fonts/Inter-Regular.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+    "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+    "Inter-Light": require("../assets/fonts/Inter-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) return null;
+
+  if (!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
