a527adac3d3ffbce09d110dd285da08c5e1c0508
Matthew Ashok <Matthewash123@gmail.com>
2024-09-30 21:56:27 +0800

    import fixes

--- frontend/app/(member_guest)/coursePage.tsx
+++ frontend/app/(member_guest)/coursePage.tsx
@@ -1,13 +1,13 @@
 import {
-    View,
-    Text,
-    TouchableOpacity,
-    StyleSheet,
-    Image,
-    Dimensions,
-    Animated,
-    FlatList,
-    ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Dimensions,
+  Animated,
+  FlatList,
+  ScrollView,
 } from "react-native";
 import React, { useState, useEffect, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -20,173 +20,168 @@ import courseListItem from "@/types/shared/courseListItem";
 import Course from "@/types/shared/Course";
 import { useFonts } from "expo-font";
 import CourseSectionTabs from "@/components/member_guest/CourseSectionTabs";
-import CourseListItem from "@/types/shared/courseListItem";
+import CourseListItem from "@/components/CourseListItem";
 
 const CoursePage = () => {
-    const [fontsLoaded, error] = useFonts({
-        "Inter-Regular": require("@/assets/fonts/Inter-Regular.ttf"),
-        "Inter-Bold": require("@/assets/fonts/Inter-Bold.ttf"),
-        "Inter-SemiBold": require("@/assets/fonts/Inter-SemiBold.ttf"),
-        "Inter-Medium": require("@/assets/fonts/Inter-Medium.ttf"),
-        "Inter-Light": require("@/assets/fonts/Inter-Light.ttf"),
-    });
-    // selectedSection: 0 for Saved, 1 for In Progress, 2 for Completed
-    const [selectedSection, setSelectedSection] = useState(0);
-    const [courses, setCourses] = useState<{
-        savedCourses: Course[];
-        inProgressCourses: Course[];
-        completedCourses: Course[];
-    }>({
-        savedCourses: [],
-        inProgressCourses: [],
-        completedCourses: [],
-    });
+  const [fontsLoaded, error] = useFonts({
+    "Inter-Regular": require("@/assets/fonts/Inter-Regular.ttf"),
+    "Inter-Bold": require("@/assets/fonts/Inter-Bold.ttf"),
+    "Inter-SemiBold": require("@/assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Medium": require("@/assets/fonts/Inter-Medium.ttf"),
+    "Inter-Light": require("@/assets/fonts/Inter-Light.ttf"),
+  });
+  // selectedSection: 0 for Saved, 1 for In Progress, 2 for Completed
+  const [selectedSection, setSelectedSection] = useState(0);
+  const [courses, setCourses] = useState<{
+    savedCourses: Course[];
+    inProgressCourses: Course[];
+    completedCourses: Course[];
+  }>({
+    savedCourses: [],
+    inProgressCourses: [],
+    completedCourses: [],
+  });
 
-    // FILTER AND SET COURSES BASED ON COMPLETION RATE
-    useEffect(() => {
-        const savedCourses = courseListData.filter(
-            (course) => course.completionRate === 0
-        );
-        const inProgressCourses = courseListData.filter(
-            (course) => course.completionRate > 0 && course.completionRate < 1
-        );
-        const completedCourses = courseListData.filter(
-            (course) => course.completionRate === 1
-        );
-
-        setCourses({
-            savedCourses,
-            inProgressCourses,
-            completedCourses,
-        });
-    }, []);
-
-    const getCoursesToDisplay = (section: number) => {
-        switch (section) {
-            case 0:
-                return courses.savedCourses;
-            case 1:
-                return courses.inProgressCourses;
-            case 2:
-                return courses.completedCourses;
-            default:
-                return [];
-        }
-    };
-
-    // ANIMATION BETWEEN HEADERS
-    const animatedValue = useRef(new Animated.Value(0)).current;
-
-    const animateSection = (newSection: number) => {
-        Animated.timing(animatedValue, {
-            toValue: newSection,
-            duration: 200,
-            useNativeDriver: true,
-        }).start();
-        setSelectedSection(newSection);
-    };
-
-    const translateX = animatedValue.interpolate({
-        inputRange: [0, 1, 2],
-        outputRange: [0, -width, -2 * width],
-    });
-
-    const handleSelectCourse = (id: string) => {
-        // TODO: INCLUDE COURSE NAVIGATION
-        console.log("Course " + id + " Selected");
-    };
-
-    return (
-        <SafeAreaView style={styles.container}>
-            {/* AppBar */}
-            <View style={styles.appBarContainer}>
-                <Text style={styles.myCoursesHeader}>
-                    {Constants.appBarTitle}
-                </Text>
-                {/* Notification bell icon */}
-                <TouchableOpacity
-                    style={styles.notificationButton}
-                    onPress={() => {
-                        router.push("/shared/notification");
-                    }}
-                >
-                    <Image
-                        source={Constants.notifBellButton}
-                        style={styles.notificationIcon}
-                    />
-                </TouchableOpacity>
-            </View>
-            {/* Section tabs */}
-            <CourseSectionTabs
-                selectedSection={selectedSection}
-                onPress={animateSection}
-            />
-            {/* 3 Category Course List with Animated Slide */}
-            <Animated.View
-                style={[
-                    styles.animatedContainer,
-                    { transform: [{ translateX }] },
-                ]}
-            >
-                {[0, 1, 2].map((section) => (
-                    <ScrollView
-                        key={section}
-                        style={styles.courseList}
-                        showsVerticalScrollIndicator={true}
-                    >
-                        {getCoursesToDisplay(section).map((course) => (
-                            <CourseListItem
-                                key={course.id}
-                                item={course}
-                                onSelect={handleSelectCourse}
-                            />
-                        ))}
-                    </ScrollView>
-                ))}
-            </Animated.View>
-        </SafeAreaView>
+  // FILTER AND SET COURSES BASED ON COMPLETION RATE
+  useEffect(() => {
+    const savedCourses = courseListData.filter(
+      (course) => course.completionRate === 0
     );
+    const inProgressCourses = courseListData.filter(
+      (course) => course.completionRate > 0 && course.completionRate < 1
+    );
+    const completedCourses = courseListData.filter(
+      (course) => course.completionRate === 1
+    );
+
+    setCourses({
+      savedCourses,
+      inProgressCourses,
+      completedCourses,
+    });
+  }, []);
+
+  const getCoursesToDisplay = (section: number) => {
+    switch (section) {
+      case 0:
+        return courses.savedCourses;
+      case 1:
+        return courses.inProgressCourses;
+      case 2:
+        return courses.completedCourses;
+      default:
+        return [];
+    }
+  };
+
+  // ANIMATION BETWEEN HEADERS
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  const animateSection = (newSection: number) => {
+    Animated.timing(animatedValue, {
+      toValue: newSection,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+    setSelectedSection(newSection);
+  };
+
+  const translateX = animatedValue.interpolate({
+    inputRange: [0, 1, 2],
+    outputRange: [0, -width, -2 * width],
+  });
+
+  const handleSelectCourse = (id: string) => {
+    // TODO: INCLUDE COURSE NAVIGATION
+    console.log("Course " + id + " Selected");
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* AppBar */}
+      <View style={styles.appBarContainer}>
+        <Text style={styles.myCoursesHeader}>{Constants.appBarTitle}</Text>
+        {/* Notification bell icon */}
+        <TouchableOpacity
+          style={styles.notificationButton}
+          onPress={() => {
+            router.push("/shared/notification");
+          }}
+        >
+          <Image
+            source={Constants.notifBellButton}
+            style={styles.notificationIcon}
+          />
+        </TouchableOpacity>
+      </View>
+      {/* Section tabs */}
+      <CourseSectionTabs
+        selectedSection={selectedSection}
+        onPress={animateSection}
+      />
+      {/* 3 Category Course List with Animated Slide */}
+      <Animated.View
+        style={[styles.animatedContainer, { transform: [{ translateX }] }]}
+      >
+        {[0, 1, 2].map((section) => (
+          <ScrollView
+            key={section}
+            style={styles.courseList}
+            showsVerticalScrollIndicator={true}
+          >
+            {getCoursesToDisplay(section).map((course) => (
+              <CourseListItem
+                key={course.id}
+                item={course}
+                onSelect={handleSelectCourse}
+              />
+            ))}
+          </ScrollView>
+        ))}
+      </Animated.View>
+    </SafeAreaView>
+  );
 };
 
 const { width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "white",
-    },
-    appBarContainer: {
-        flexDirection: "row",
-        marginTop: 10,
-        alignItems: "center",
-        justifyContent: "space-between",
-    },
-    myCoursesHeader: {
-        color: Colors.defaultBlue,
-        fontFamily: "Inter-Regular",
-        marginLeft: 25,
-        paddingBottom: 2,
-        fontSize: 22,
-        fontWeight: "bold",
-    },
-    notificationButton: {
-        padding: 12,
-        alignItems: "center",
-        justifyContent: "center",
-        marginRight: 12,
-    },
-    notificationIcon: {
-        height: 32,
-        width: 32,
-    },
-    animatedContainer: {
-        flexDirection: "row",
-        width: width * 3,
-        flex: 1,
-    },
-    courseList: {
-        width,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  appBarContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  myCoursesHeader: {
+    color: Colors.defaultBlue,
+    fontFamily: "Inter-Regular",
+    marginLeft: 25,
+    paddingBottom: 2,
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  notificationButton: {
+    padding: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 12,
+  },
+  notificationIcon: {
+    height: 32,
+    width: 32,
+  },
+  animatedContainer: {
+    flexDirection: "row",
+    width: width * 3,
+    flex: 1,
+  },
+  courseList: {
+    width,
+  },
 });
 
 export default CoursePage;
--- frontend/app/(member_guest)/suggestionsSeeAll.tsx
+++ frontend/app/(member_guest)/suggestionsSeeAll.tsx
@@ -1,10 +1,10 @@
 import {
-    View,
-    Text,
-    TouchableOpacity,
-    StyleSheet,
-    Image,
-    FlatList,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  FlatList,
 } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -12,102 +12,90 @@ import { useFonts } from "expo-font";
 import { router, useLocalSearchParams } from "expo-router";
 
 import { Colors } from "@/constants/colors";
-import CourseListItem from "@/types/shared/courseListItem";
+import CourseListItem from "@/components/CourseListItem";
 import Course from "@/types/shared/Course";
 import { memberGuestSuggestionsSeeAll as Constants } from "@/constants/textConstants";
 
 const SuggestionsSeeAll = () => {
-    const [fontsLoaded, error] = useFonts({
-        "Inter-Regular": require("@/assets/fonts/Inter-Regular.ttf"),
-        "Inter-Bold": require("@/assets/fonts/Inter-Bold.ttf"),
-        "Inter-SemiBold": require("@/assets/fonts/Inter-SemiBold.ttf"),
-        "Inter-Medium": require("@/assets/fonts/Inter-Medium.ttf"),
-        "Inter-Light": require("@/assets/fonts/Inter-Light.ttf"),
-    });
+  const [fontsLoaded, error] = useFonts({
+    "Inter-Regular": require("@/assets/fonts/Inter-Regular.ttf"),
+    "Inter-Bold": require("@/assets/fonts/Inter-Bold.ttf"),
+    "Inter-SemiBold": require("@/assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Medium": require("@/assets/fonts/Inter-Medium.ttf"),
+    "Inter-Light": require("@/assets/fonts/Inter-Light.ttf"),
+  });
 
-    const { suggestions } = useLocalSearchParams();
-    const parsedSuggestions: Course[] =
-        typeof suggestions === "string" ? JSON.parse(suggestions) : [];
+  const { suggestions } = useLocalSearchParams();
+  const parsedSuggestions: Course[] =
+    typeof suggestions === "string" ? JSON.parse(suggestions) : [];
 
-    const handleSelectCourse = (id: string) => {
-        // TODO: INCLUDE COURSE NAVIGATION
-        console.log("Course " + id + " Selected");
-    };
+  const handleSelectCourse = (id: string) => {
+    // TODO: INCLUDE COURSE NAVIGATION
+    console.log("Course " + id + " Selected");
+  };
 
-    return (
-        <SafeAreaView style={styles.container}>
-            {/* AppBar */}
-            <View style={styles.appBarContainer}>
-                <TouchableOpacity
-                    onPress={() => {
-                        router.back();
-                    }}
-                >
-                    <Image
-                        source={Constants.backButton}
-                        style={styles.backButton}
-                    />
-                </TouchableOpacity>
-                <Text style={styles.suggestionsHeader}>
-                    {Constants.appBarTitle}
-                </Text>
-            </View>
-            {/* Suggestions Display section */}
-            <View>
-                <View style={styles.listContainer}>
-                    <FlatList
-                        data={parsedSuggestions}
-                        renderItem={({ item }) => (
-                            <CourseListItem
-                                item={item}
-                                onSelect={handleSelectCourse}
-                            />
-                        )}
-                        keyExtractor={(item) => item.id}
-                        ListHeaderComponent={
-                            <View style={styles.headerFooterSpacing} />
-                        }
-                        ListFooterComponent={
-                            <View style={styles.headerFooterSpacing} />
-                        }
-                    />
-                </View>
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* AppBar */}
+      <View style={styles.appBarContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <Image source={Constants.backButton} style={styles.backButton} />
+        </TouchableOpacity>
+        <Text style={styles.suggestionsHeader}>{Constants.appBarTitle}</Text>
+      </View>
+      {/* Suggestions Display section */}
+      <View>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={parsedSuggestions}
+            renderItem={({ item }) => (
+              <CourseListItem item={item} onSelect={handleSelectCourse} />
+            )}
+            keyExtractor={(item) => item.id}
+            ListHeaderComponent={<View style={styles.headerFooterSpacing} />}
+            ListFooterComponent={<View style={styles.headerFooterSpacing} />}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "white",
-    },
-    appBarContainer: {
-        flexDirection: "row",
-        marginTop: 20,
-        alignItems: "center",
-    },
-    backButton: {
-        height: 25,
-        width: 25,
-        marginLeft: 25,
-        padding: 5,
-    },
-    suggestionsHeader: {
-        color: Colors.defaultBlue,
-        fontFamily: "Inter-Regular",
-        marginLeft: 25,
-        paddingBottom: 2,
-        fontSize: 22,
-        fontWeight: "bold",
-    },
-    listContainer: {
-        flexDirection: "row",
-        marginTop: 30,
-    },
-    headerFooterSpacing: {
-        width: 12,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  appBarContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+    alignItems: "center",
+  },
+  backButton: {
+    height: 25,
+    width: 25,
+    marginLeft: 25,
+    padding: 5,
+  },
+  suggestionsHeader: {
+    color: Colors.defaultBlue,
+    fontFamily: "Inter-Regular",
+    marginLeft: 25,
+    paddingBottom: 2,
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  listContainer: {
+    flexDirection: "row",
+    marginTop: 30,
+  },
+  headerFooterSpacing: {
+    width: 12,
+  },
 });
 
 export default SuggestionsSeeAll;
