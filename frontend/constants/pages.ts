7b7e87f18008c159efddb4a0a2bf76f86e4f0e9e
Matthew Ashok <Matthewash123@gmail.com>
2024-12-31 21:42:15 +0800

    [matt] Donation page is now shared

--- frontend/constants/pages.ts
+++ frontend/constants/pages.ts
@@ -20,6 +20,7 @@ export const INSTRUCTOR_WAITING_PAGE =
   "/shared/instructorWaitingPage" as Href<string>;
 export const MEMBER_REGISTRATION_SUCCESS =
   "/shared/registrationSuccessMemberPage" as Href<string>;
+export const DONATION_PAGE = "/shared/profile/donate" as Href<string>;
 
 // MEMBER/GUEST PAGES
 export const MEMBER_GUEST_NAMESPACE = "(member_guest)";
